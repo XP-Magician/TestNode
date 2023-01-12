fbd30cff0b64202dddee0dcf6c9a86e92ea280e1
Diego <diegofabrega18@gmail.com>
2023-01-11 23:10:00 -0300

    respuesta con json

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const http=require('http') //Especifico utilizacion de API para trabajar con HTTP
 
+let datos = [     //Pseudo base de datos
+    {
+        'name':'Diego',
+         'age':21
+    },
+    {
+        'name':'Jose',
+         'age':20
+    },
+    {
+        'name':'Maria',
+         'age':25
+    },
+    {
+        'name':'Javiera',
+         'age':24
+    }
+]
+
+
 const app = http.createServer((request,response)=>{  //Cada vez que reciba una peticion, callback a response:
-    response.writeHead(200,{'Content-Type':'text/plain'}) //Response = estado de peticion 200 (exitoso),cabecera
-                                                         // que especifica el tipo de dato devuelto (!importante)
-    response.end("Hello world") // Termina la respuesta y devuelve el parametro entregado
+    response.writeHead(200,{'Content-Type':'application/json'}) /*Response = estado de peticion 200 (exitoso),cabecera
+    que especifica el tipo de dato devuelto (!importante) */
+    response.end(JSON.stringify(datos)) // Termina la respuesta y devuelve el parametro entregado
+    //parsea el json para que sea legible
 })
 
 const PORT=3001 // especifica el puerto en que se ejecutara el server, usualmente es el 80 que por ser tan comun
 //no se especifica 
 
 app.listen(PORT) //le decimos al server que escuche en el puerto especifico que creamos anteriormente
-console.log("Server running on port : $(PORT)") //Enviamos un mensaje al ejecutar el servidor
+console.log('Server running on port $(PORT)') //Enviamos un mensaje al ejecutar el servidor
