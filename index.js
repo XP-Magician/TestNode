89aa2f5253b27b7d843515a3078be0e597d71ea1
Diego <diegofabrega18@gmail.com>
2023-01-12 22:17:52 -0300

    ADD: Se agrego path para HTTP DELETE en datos

--- index.js
+++ index.js
@@ -6,18 +6,22 @@ const app = express() // la instancia o app del modulo express
 
 let datos = [     //Pseudo base de datos
     {
+        'id':1,
         'name':'Diego',
          'age':21
     },
     {
+        'id':2,
         'name':'Jose',
          'age':20
     },
     {
+        'id':3,
         'name':'Maria',
          'age':25
     },
     {
+        'id':4,
         'name':'Javiera',
          'age':24
     }
@@ -31,13 +35,26 @@ app.get('/datos',(request,response)=>{  // en una peticion tipo get en el path (
    response.json(datos)
 })
 
+app.get('/datos/:id',(request,response)=>{
+    const id=Number(request.params.id) //recupero el id de los parametros enviados en el request, lo parseo
+    const note= datos.find(notes=>notes.id===id) // encuentro solo la nota correspondiente al id solicitado
+    note!=undefined?response.json(note):response.status(404).end() // si existe devuelvo la nota y sino 404
+})
+
+app.delete('/datos/:id',(request,response)=>{
+    const id=Number(request.params.id) //recupero el id de los parametros enviados en el request, lo parseo
+    datos=datos.filter(notes=>notes.id!==id)
+    response.status(202).end();
+})
+
 
 
 const PORT=3001 // especifica el puerto en que se ejecutara el server, usualmente es el 80 que por ser tan comun
 //no se especifica 
 
 app.listen(PORT,()=>{
-    console.log("server running on port ".request.PORT)
+
+    console.log("server running on port ".PORT)
 }) 
 
 /*al igual que el server anterior, se utiliza el metodo listen y el puerto personalizado
